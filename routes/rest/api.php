989119346ee92deb4935287ec1bafa8cd39eeff1
zamandevs <32278210+zamandevs@users.noreply.github.com>
2026-02-10 11:51:01 +0600

    Remove deprecated block preview REST pipeline and hook

--- routes/rest/api.php
+++ routes/rest/api.php
@@ -2,16 +2,6 @@
 
 defined( 'ABSPATH' ) || exit;
 
-use DirectoristGutenberg\App\Http\Controllers\BlocksPreviewTemplateController;
-use DirectoristGutenberg\WpMVC\Routing\Route;
-
 include __DIR__ . '/admin.php';
-
-Route::group( 'blocks-preview', function() {
-    
-    Route::get( 'listings-archive/{block_type}', [ BlocksPreviewTemplateController::class, 'listings_archive_blocks_preview' ] );
-
-}, [] );
-
 
 
